c72cea4ffdcdfba1f6cbe3335ab403f8636ffe70
Hugo Rafael <98796954+hugorafaelll@users.noreply.github.com>
2022-09-06 13:11:47 -0300

    hooks
    useState e useEfect

--- 03firebase/src/App.js
+++ 03firebase/src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import axios from 'axios';
 
 
@@ -9,19 +9,37 @@ import axios from 'axios';
 // }) 
 
 
-axios
-.post('https://financas-hugo-default-rtdb.firebaseio.com/valor.json', 
-{
-  outroDado :'hugo Rafael '
-})
-.then(res =>{
-  console.log(res.data)
-})
+// axios
+// .post('https://financas-hugo-default-rtdb.firebaseio.com/valor.json', 
+// {
+//   outroDado :'hugo Rafael '
+// })
+// .then(res =>{
+//   console.log(res.data)
+// })
+
+const url = 'https://financas-hugo-default-rtdb.firebaseio.com/financas/movimentacao.json'
 
 function App() {
+
+  const [loading, setLoading] = useState(false)
+  const [data, setData] = useState({})   //hook para mostrar na tela as "fotos"
+  useEffect( ()=>{ //hook para executar uma ação 
+    setLoading(true)
+    axios  // api firebase para trazer DATA
+    .get(url)  // local da api
+    .then(res =>{    //assyn com resposta da data no console
+      setData(res.data)
+      setLoading(false)
+    })
+  }, [] ) // []quer dizer que nao precisa de ningm para executar esta ação
+
   return (
     <div >
       <h1>Financas</h1>
+      {JSON.stringify(data)}
+     { loading && <p>CARREGANDO...</p>}
+          
     </div>
   );
 }
