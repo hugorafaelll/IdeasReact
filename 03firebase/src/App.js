700585e670a94986cff8a4e50bceae23ec80ea5d
Hugo Rafael <98796954+hugorafaelll@users.noreply.github.com>
2022-09-06 12:16:30 -0300

    testAxios
    testando o api do firebase na aplicação

--- 03firebase/src/App.js
+++ 03firebase/src/App.js
@@ -2,12 +2,21 @@ import React from "react";
 import axios from 'axios';
 
 
-axios
-.get('https://financas-hugo-default-rtdb.firebaseio.com/valor.json')
-.then(res => {
-  console.log(res)
-}) 
+// axios
+// .get('https://financas-hugo-default-rtdb.firebaseio.com/valor.json')
+// .then(res => {
+//   console.log(res.data.chave)
+// }) 
 
+
+axios
+.post('https://financas-hugo-default-rtdb.firebaseio.com/valor.json', 
+{
+  outroDado :'hugo Rafael '
+})
+.then(res =>{
+  console.log(res.data)
+})
 
 function App() {
   return (
