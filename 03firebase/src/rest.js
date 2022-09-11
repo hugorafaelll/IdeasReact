370a7c351b7792a60efc2da25bdc5a8db7bcb0dd
Hugo Rafael <98796954+hugorafaelll@users.noreply.github.com>
2022-09-11 12:59:36 -0300

    rest.js

--- 03firebase/src/rest.js
+++ 03firebase/src/rest.js
@@ -1,93 +1,41 @@
-import { useReducer,useEffect } from "react";
-import axios from "axios";
+import { useEffect, useReducer } from "react"
+import axios from "axios"
 
-//reducer se repete bastante
-
-const baseUrl = 'https://financas-hugo-default-rtdb.firebaseio.com/financas'
-
-const reducer = (state, action) => {  // função para manipular um estado  
+const reducer = ( state, action) => {
     if(action.type === 'REQUEST'){
       return {
         ...state,
         loading:true
       }
     }
-    if(action.type === 'SUCCESS')
-    return{
-      ...state,
-      loading:false,
-      action: action.data
+    if(action.type === 'SUCCESS'){
+      return{
+        ...state,
+        loading:false,
+        data:action.data
       }
-      return state
     }
+    return state
+  }
 
+  const baseUrl = 'https://financas-hugo-default-rtdb.firebaseio.com/'
 
-const init = baseUrl => {
-
-    const useGet = resource =>{
-        const [data, dispatch] = useReducer( reducer,{
-          loading:true,
-          data:{}
-        })
-        useEffect( ()=>{ //hook para executar uma ação 
-          dispatch({type:'REQUEST'})
-          axios   // api firebase para trazer DATA
-          .get(baseUrl+resource+'.json')  // local da api
-          .then(res =>{    //assyn com resposta da data no console
-              dispatch ({type:'SUCCESS', data: res.data})
-          })
-        } ) // []quer dizer que nao precisa de ningm para executar esta ação
-        return data
-    } 
-
-    const usePost = (resource) => {
-    const [data, dispatch] = useReducer( reducer,{
-        loading:false,
-        data:{}
-      })
-    const post = data =>{
-        dispatch ({type:'REQUEST'})
+  const useGet = url => {
+    const [data, dispatch] = useReducer(reducer,{
+      loading:true,
+      data:{}
+    })
+    useEffect(() =>{
+        dispatch({type:'REQUEST'})
         axios
-            .post(baseUrl+resource+'.json', data)
-            .then(res =>{
-                dispatch ({
-                type:'SUCCESS',
-                data: res.data
-            })
+        .get(baseUrl+ resource +'.json')
+        .then(res => {
+          dispatch({type:'SUCCESS', data:res.data})
         })
-    } 
-    return [data , post]
-}
+  
+   
+    },[]) //> [] significa que e independente e vai rodar apenas da primeira vez
+      return data // quem eu quero mostrar do lado de fora 
+  }
 
-const useDelete = () => {
-
-    const [data, dispatch] = useReducer( reducer,{
-        loading:false,
-        data:{}
-      })
-      
-
-    const remove = resource =>{
-        dispatch ({type:'REQUEST'})
-        axios
-            .delete(baseUrl+resource+'.json')
-            .then(()=>{
-                dispatch ({
-                type:'SUCCESS',
-              
-            })
-        })
-    } 
-    return [data , remove]
-}
-
-
-        return {
-            useGet,
-            usePost,
-            useDelete
-        }
-}
-
-
-export default init
+  export default useGet
