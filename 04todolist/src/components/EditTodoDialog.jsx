140da182cb09946330b9f037d977b2b03dd33f7b
Hugo Rafael <98796954+hugorafaelll@users.noreply.github.com>
2022-09-23 11:11:54 -0300

    fixed

--- 04todolist/src/components/EditTodoDialog.jsx
+++ 04todolist/src/components/EditTodoDialog.jsx
@@ -29,7 +29,7 @@ export default function EditTodoDialog({ open, dialogHandler, todo, editTodo }) 
       aria-describedby="alert-dialog-slide-description"
       fullWidth
     >
-      <DialogTitle>{"Editando TAREFAAAA"}</DialogTitle>
+      <DialogTitle>{"Edit"}</DialogTitle>
       <DialogContent>
         <TextField
           defaultValue={editedText}
--- 04todolist/src/components/form.jsx
+++ 04todolist/src/components/form.jsx
@@ -17,7 +17,7 @@ export default function Form({ addTodo }) {
       <div style={{ display: "flex", justifyContent: "center" }}>
         <TextField
           id="outlined-basic"
-          label="Tarefa"
+          label="Task"
           variant="outlined"
           onChange={(e) => setText(e.target.value)}
           fullWidth
