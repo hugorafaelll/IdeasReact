20daf0da7573ef65b01829e4f7c72b839ceb3aa3
Hugo Rafael <98796954+hugorafaelll@users.noreply.github.com>
2022-09-23 16:06:16 -0300

    creating drag and drop menu for add in task and transform  in  kanbam

--- 05trello/src/App.js
+++ 05trello/src/App.js
@@ -1,13 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
+const inicialColumns = [
+  {
+    name: "todo",
+    id: "123",
+  },
+];
 
-
-
-
+const items = [
+  {
+    id: "1",
+    conteudo: "conteudo 1",
+    id: "2",
+    conteudo: "conteudo 2",
+    id: "3",
+    conteudo: "conteudo 3",
+    id: "4",
+    conteudo: "conteudo 4",
+  },
+];
 
 function App() {
+  const [columns, setColumns] = useState(inicialColumns);
+
   return (
-    <div className="App">
-    
+    <div>
+      {inicialColumns.map((column) => (
+        <div style={{display:"flex", flexDirection:"column", alignItems:'center'}}>
+          <h1>{column.name}</h1>
+          <div style={{backgroundColor:'black', width:250,height:500}}>
+              {column.items.map((iten) => (
+                <div>texto</div>
+              ))}
+          </div>
+          
+        </div>
+      ))}
     </div>
   );
 }
